e3327e8da49bc99cd88c0ae19d01377407786d07
issa alhadithi <essaalzien@gmail.com>
2017-03-13 18:58:15 +0300

    mysite

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+    'use strict';
+    $('.slider').slick({
+        prevArrow : " ",   
+        nextArrow : " ",
+        autoplay : true,
+        autoplaySpeed : 5000
+    });
+    
+});
